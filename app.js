a7a281fecad18e1a2439f26ca6c0d182fa374d0e
Brian Harkins <brianharkins@CN0405.local>
2024-06-06 11:01:54 +0100

    imported sounds and added simple functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("hello there!")
+
+let hihatSound = new Audio("./sounds/hihat.wav");
+let clapsound = new Audio("./sounds/clap.wav");
+//do for each sound
+
+
+const hihat = document.getElementById('hihat')
+
+hihat.addEventListener('click', () => {
+    hihatSound.play()
+})
